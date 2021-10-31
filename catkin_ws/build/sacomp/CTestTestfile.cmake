52b16347825d48296625b610773a13ffec830a7d
kriskappel <kristofer.kappel@gmail.com>
2021-10-31 17:05:54 -0300

    deleted files from old version

--- catkin_ws/build/sacomp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kriskappel/Documents/MiniCursoRos/catkin_ws/src/sacomp
-# Build directory: /home/kriskappel/Documents/MiniCursoRos/catkin_ws/build/sacomp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
